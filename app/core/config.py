0680e1a911dc3794e256d5f676e8b22d3d08981d
ParthivDataScientist <partivpatel684@gmail.com>
2026-05-13 21:22:36 +0530

    say no more

--- app/core/config.py
+++ app/core/config.py
@@ -70,6 +70,39 @@ class Settings(BaseSettings):
     DHL_WCF_SOAP_ACTION: str = "http://tempuri.org/IDHLService/PostTracking"
     DHL_WCF_SOAP_VERSION: Literal["1.1", "1.2"] = "1.1"
     DHL_WCF_TIMEOUT_SECONDS: int = 20
+
+    # DHL booking / pickup credentials
+    DHL_SITE_ID: str = ""
+    DHL_SHIPPER_ID: str = ""
+    DHL_SHIPPER_ACCOUNT_NUMBER: str = ""
+    DHL_BILLING_ACCOUNT_NUMBER: str = ""
+    DHL_DUTY_ACCOUNT_NUMBER: str = ""
+
+    # DHL shipper defaults used when creating shipments and pickups.
+    DHL_SHIPPER_COMPANY: str = ""
+    DHL_SHIPPER_NAME: str = ""
+    DHL_SHIPPER_ADDRESS1: str = ""
+    DHL_SHIPPER_ADDRESS2: str = ""
+    DHL_SHIPPER_ADDRESS3: str = ""
+    DHL_SHIPPER_CITY: str = ""
+    DHL_SHIPPER_POSTAL_CODE: str = ""
+    DHL_SHIPPER_COUNTRY_CODE: str = "IN"
+    DHL_SHIPPER_COUNTRY_NAME: str = "India"
+    DHL_SHIPPER_PHONE: str = ""
+
+    # DHL booking defaults
+    DHL_DEFAULT_PRODUCT_CODE: str = "P"
+    DHL_DEFAULT_LOCAL_PRODUCT_CODE: str = "P"
+    DHL_DEFAULT_NETWORK_TYPE_CODE: str = "AL"
+    DHL_DEFAULT_SPECIAL_SERVICE: str = ""
+    DHL_DEFAULT_SHIPPING_PAYMENT_TYPE: str = "S"
+    DHL_DEFAULT_DUTY_PAYMENT_TYPE: str = "R"
+    DHL_DEFAULT_SHIP_CURRENCY: str = "INR"
+    DHL_DEFAULT_DECLARED_CURRENCY: str = "INR"
+    DHL_DEFAULT_TERMS_OF_TRADE: str = "DAP"
+    DHL_DEFAULT_PICKUP_READY_TIME: str = "10:00"
+    DHL_DEFAULT_PICKUP_CLOSE_TIME: str = "18:00"
+    DHL_DEFAULT_PICKUP_LOCATION: str = ""
     
     STORAGE_DIR: str = os.path.join(_ROOT_DIR, "storage")
     DHL_LABELS_SUBDIR: str = "labels"
